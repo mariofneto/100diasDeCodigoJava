cff43ab08a21354cd70c5d7115acb3c741519cc9
Mário Neto <marioo.netoo3@gmail.com>
2025-03-11 21:06:54 -0300

    iniciando 100 dias de estudo java

--- introducao/CondicaoTernaria.java
+++ introducao/CondicaoTernaria.java
@@ -10,5 +10,7 @@ public class CondicaoTernaria {
         String resposta = (a > 20) ? String.format("O número %d é maior que 20", a): String.format("O número %d é menor que 20", a);
 
         System.out.println(resposta);
+
+        sc.close();
     }
 }
